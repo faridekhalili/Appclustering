5da36222ace8a9bb26d68fd60ceba68b9da36c14
Faride Khalili <faridekhalili1393@gmail.com>
2020-11-13 22:09:10 +0330

    Add print statements

--- topic_modeling.py
+++ topic_modeling.py
@@ -5,31 +5,44 @@ from word2vec import *
 from abc import ABC, abstractmethod
 import pickle
 
+with open("./output/timings.txt", "w") as f:
+    sys.stdout = f
+    print('This message will be written to a file.')
+    sys.stdout = original_stdout
+
 
 class TopicModel(ABC):
     def __init__(self, dataset, folder_path, algorithm):
+        print(init)
         self.max_num_topics = 100
         self.dataset = dataset
         self.folder_path = folder_path
         self.algorithm = algorithm
         self.dictionary = gensim.corpora.Dictionary(self.dataset)
+        print("dictionary created")
         bow_corpus = [self.dictionary.doc2bow(doc) for doc in self.dataset]
+        print("bow_corpus created")
         tfidf = gensim.models.TfidfModel(bow_corpus)
+        print("tfidf model created")
         self.tfidf = tfidf
         self.corpus_tfidf = tfidf[bow_corpus]
+        print("init done")
         super().__init__()
 
     def __save_dictionary(self):
         dictionary_path = self.folder_path + "dataset.dict"
         pickle_save(self.dictionary, dictionary_path)
+        print("__save_dictionary")
 
     def __save_tfidf_model(self):
         tfidf_path = self.folder_path + "new_dataset.tfidf_model"
         pickle_save(self.tfidf, tfidf_path)
+        print("__save_tfidf_model")
 
     def __plot_coherence_scores(self, coherence_scores):
         figure_path = self.folder_path + self.algorithm + '/' + self.algorithm + '_coherence.png'
         save_coherence_plot(self.max_num_topics, coherence_scores, figure_path)
+        print("__plot_coherence_scores")
 
     def __extract_dominant_topics(self, best_model):
         topic_clusters = []
@@ -42,10 +55,11 @@ class TopicModel(ABC):
                 dominant_topic = max(topic_distribution, key=topic_distribution.get)
                 topic_clusters.append(dominant_topic)
         self.dataset = [i for j, i in enumerate(self.dataset) if j not in remove_indices]
-
+        print("__extract_dominant_topics")
         return topic_clusters
 
     def __extract_word2vec_models(self, df):
+        sys.stdout = f
         distribution_plot_path = self.folder_path + self.algorithm + '/topic_distribution.png'
         plot_distribution(df, distribution_plot_path, 'topic')
         count = 0
@@ -58,15 +72,18 @@ class TopicModel(ABC):
             word2vec_trainer(df=df_category, model_path=model_name)
             print("Time taken to train the word2vec model: " + str(time.time() - start_time) + '\n')
         print("Time taken to train all the word2vec models: " + str(time.time() - start_all_time) + '\n')
-        print(80*"#")
+        print(80 * "#")
+        sys.stdout = original_stdout
 
     def save_topic_model(self, model):
         model_path = self.folder_path + self.algorithm + '/model/' + self.algorithm + '.model'
         model.save(model_path)
+        print("save_topic_model")
 
     def save_dict_and_tfidf(self):
         self.__save_dictionary()
         self.__save_tfidf_model()
+        print("save_dict_and_tfidf")
 
     def search_num_of_topics(self):
         coherence_scores = []
@@ -80,12 +97,14 @@ class TopicModel(ABC):
         pprint(best_model.print_topics())
         self.save_topic_model(best_model)
         self.__plot_coherence_scores(coherence_scores)
+        print("search_num_of_topics")
         return best_model
 
     def divide_into_clusters(self, best_model):
         topic_clusters = self.__extract_dominant_topics(best_model)
         extended_df = pd.DataFrame(list(zip(self.dataset, topic_clusters)), columns=['description', 'topic'])
         self.__extract_word2vec_models(extended_df)
+        print("divide_into_clusters")
 
     def topic_prob_extractor(self, model):
         shown_topics = model.print_topics(num_topics=150, num_words=500)
@@ -97,6 +116,7 @@ class TopicModel(ABC):
         df.drop(index_names, inplace=True)
         topic_wight_df_path = self.folder_path + self.algorithm + '/topic_wight_df.csv'
         df.to_csv(topic_wight_df_path)
+        print("topic_prob_extractor")
         return df
 
     @abstractmethod
@@ -114,7 +134,9 @@ class LSA(TopicModel):
         lsa_model = gensim.models.LsiModel(self.corpus_tfidf,
                                            num_topics=num_topics,
                                            id2word=self.dictionary)
-        print("Time taken to train the lsa model: " + str(time.time() - start_time)+'\n')
+        sys.stdout = f
+        print("Time taken to train the lsa model: " + str(time.time() - start_time) + '\n')
+        sys.stdout = original_stdout
         return lsa_model
 
 
@@ -129,7 +151,9 @@ class LDA(TopicModel):
                                                num_topics=num_topics,
                                                id2word=self.dictionary,
                                                passes=4, workers=10, iterations=100)
-        print("Time taken to train the lda model: " + str(time.time() - start_time)+'\n')
+        sys.stdout = f
+        print("Time taken to train the lda model: " + str(time.time() - start_time) + '\n')
+        sys.stdout = original_stdout
         return lda_model
 
 
@@ -142,7 +166,9 @@ class HDP(TopicModel):
         start_time = time.time()
         hdp_model = gensim.models.hdpmodel.HdpModel(corpus=self.corpus_tfidf, id2word=self.dictionary)
         pprint(best_model.print_topics(num_words=10))
-        print("Time taken to train the hdp model: " + str(time.time() - start_time)+'\n')
+        sys.stdout = f
+        print("Time taken to train the hdp model: " + str(time.time() - start_time) + '\n')
+        sys.stdout = original_stdout
         self.save_topic_model(hdp_model)
         self.topic_prob_extractor.topic_prob_extractor(hdp_model)
         return hdp_model
@@ -165,7 +191,6 @@ def pickle_save(my_model, file_name):
 
 
 def main():
-    sys.stdout = open("./output/timings.txt", "w")
     conf = toml.load('config.toml')
     topic_modeling_path = conf['topic_modeling_path']
     df = pd.read_csv(conf["preprocessed_data_path"])
@@ -175,13 +200,16 @@ def main():
     lsa_obj.save_dict_and_tfidf()
     best_lsa_model = lsa_obj.search_num_of_topics()
     lsa_obj.divide_into_clusters(best_lsa_model)
+    del lsa_obj
 
     lda_obj = LDA(texts, topic_modeling_path, "lda")
     best_lda_model = lda_obj.search_num_of_topics()
     lda_obj.divide_into_clusters(best_lda_model)
+    del lda_obj
 
     hdp_obj = HDP(texts, topic_modeling_path, "hdp")
     hdp_model = hdp_obj.get_model()
+    del hdp_obj
 
 
 if __name__ == "__main__":
