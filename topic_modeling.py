2d8c6f0c042928216167b5172d3c6f9f2902262c
Faride Khalili <faridekhalili1393@gmail.com>
2020-11-26 13:00:36 +0000

    resolving conflicts

--- topic_modeling.py
+++ topic_modeling.py
@@ -148,6 +148,7 @@ def main():
     print("reading df")
     df = pd.read_csv(conf["preprocessed_data_path"])
     print("df read")
+    print(df.columns)
     texts = [literal_eval(x) for x in list(df["description"])]
     print("texts created")
     del df
