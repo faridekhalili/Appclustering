d4f1360ccbba2ae3249f4817080223e1560b49b3
Faride Khalili <faridekhalili1393@gmail.com>
2021-01-04 15:06:54 +0330

    retrieve all word embedding models together

--- best_word_embedding_model_finder.py
+++ best_word_embedding_model_finder.py
@@ -7,7 +7,7 @@ import re
 from ast import literal_eval
 
 
-def get_dominant_topic(new_docs, dictionary, tfidf, topic_model, models_path):
+def get_dominant_topic(new_docs, dictionary, tfidf, topic_model):
     for doc in list(new_docs):
         vec_bow = dictionary.doc2bow(doc)
         vec_tfidf = tfidf[vec_bow]
@@ -17,11 +17,10 @@ def get_dominant_topic(new_docs, dictionary, tfidf, topic_model, models_path):
         else:
             topic_distribution = dict(topic_model[vec_tfidf])
             dominant_topic = max(topic_distribution, key=topic_distribution.get)
-            best_model = models_path + "w2v_model_" + str(dominant_topic)
-    return best_model
+    return dominant_topic
 
 
-def get_best_word2vec_model(algorithm, query_doc, path):
+def get_best_word_embedding_model(algorithm, query_doc, path):
     with open(open(path + "dictionary"), 'rb') as pickle_file:
         dictionary = pickle.load(pickle_file)
     with open(open(path + "tfidf_model"), 'rb') as pickle_file:
@@ -32,30 +31,21 @@ def get_best_word2vec_model(algorithm, query_doc, path):
     df["description"] = pre_process(df[['description']])
     preprocessed_application_description = [literal_eval(x) for x in list(df["description"])]
 
-    if algorithm == "lda":
-        model_path = path + 'lda/model/lda.model'
-        with open(open(model_path), 'rb') as pickle_file:
-            lda_model = pickle.load(pickle_file)
-        word2vec_models_path = path + 'lda/word2vec_models/'
-        word2vec_model = get_dominant_topic(preprocessed_application_description, dictionary,
-                                            tfidf, lda_model, word2vec_models_path)
-    elif algorithm == "lsa":
-        model_path = path + 'lsa/model/lsa.model'
-        with open(open(model_path), 'rb') as pickle_file:
-            lsa_model = pickle.load(pickle_file)
-        word2vec_models_path = path + 'lsa/word2vec_models/'
-        word2vec_model = get_dominant_topic(preprocessed_application_description,
-                                                 dictionary, tfidf, lsa_model,
-                                                 word2vec_models_path)
-    elif algorithm == "hdp":
-        model_path = path + 'hdp/model/hdp.model'
-        with open(open(model_path), 'rb') as pickle_file:
-            hdp_model = pickle.load(pickle_file)
-        word2vec_models_path = path + 'hdp/word2vec_models/'
-        word2vec_model = get_dominant_topic(preprocessed_application_description,
-                                            dictionary, tfidf, hdp_model,
-                                            word2vec_models_path)
-    return word2vec_model
+    model_path = path + algorithm + "model/" + algorithm + ".model"
+    with open(open(model_path), 'rb') as pickle_file:
+        lda_model = pickle.load(pickle_file)
+    dominant_topic = get_dominant_topic(preprocessed_application_description, dictionary,
+                                        tfidf, lda_model)
+    w2v_model_path = path + algorithm + '/word2vec_models/' + "w2v_model_" + str(dominant_topic)
+    with open(open(w2v_model_path), 'rb') as pickle_file:
+        w2v_model = pickle.load(pickle_file)
+    fast_text_model_path = path + algorithm + '/word2vec_models/' + "model_" + str(dominant_topic)
+    with open(open(fast_text_model_path), 'rb') as pickle_file:
+        fast_text_model = pickle.load(pickle_file)
+    glove_model_path = path + algorithm + '/word2vec_models/' + "model_" + str(dominant_topic)
+    with open(open(glove_model_path), 'rb') as pickle_file:
+        glove_model = pickle.load(pickle_file)
+    return w2v_model, fast_text_model, glove_model
 
 
 def check_inputs(args):
@@ -84,9 +74,12 @@ def main():
     flag, args = check_inputs(args)
     if flag:
         return
-    model = get_best_word2vec_model(args.algorithm, query_input_path + args.file_name,
-                                    topic_modeling_path)
-    pickle.dump(model, open(query_result_path + args.file_name[:-4] + "_w2v_model", "wb"))
+    w2v_model, fast_text_model, glove_model = get_best_word_embedding_model(args.algorithm,
+                                                                            query_input_path + args.file_name,
+                                                                            topic_modeling_path)
+    pickle.dump(w2v_model, open(query_result_path + "word2vec" + args.file_name[:-4] + "_w2v_model", "wb"))
+    pickle.dump(fast_text_model, open(query_result_path + "FastText" + args.file_name[:-4] + "_model", "wb"))
+    pickle.dump(glove_model, open(query_result_path + "Glove" + args.file_name[:-4] + "_model", "wb"))
 
 
 if __name__ == "__main__":
